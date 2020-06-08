1d59682a56badc248bbbc1d989a606a0bbb54230
katlany <notan8@gmail.com>
2020-06-08 08:00:27 +0300

    add favorites reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import ui from "./ui";
 import weatherReducer from "./weather-reducer";
+import favoritesReducer from "./favorites-reducer";
 export const reducers = combineReducers({
   ui,
   weatherReducer,
+  favoritesReducer,
 });
