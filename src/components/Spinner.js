ff1d2adb0bac4b0099581fa58a2c79f2e5c82a12
katlany <notan8@gmail.com>
2020-06-11 06:52:03 +0300

    style up spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
-const Spinner = ({ loading }) => {
+const Spinner = ({ loading, themeMode }) => {
+  const theme = themeMode === "dark" ? "text-light" : "text-dark";
   const loadingUi = loading ? (
     <div className="text-center">
       <div
-        style={{ width: "20px", height: "20px" }}
-        className="spinner-border text-primary"
+        style={{ marginTop: "40px", width: "40px", height: "40px" }}
+        className={"spinner-border " + theme}
         role="status"
       >
         <span className="sr-only">Loading...</span>
@@ -14,9 +15,10 @@ const Spinner = ({ loading }) => {
   ) : null;
   return loadingUi;
 };
-const mapStateToProps = ({ ui: { loading } }) => {
+const mapStateToProps = ({ ui: { loading, themeMode } }) => {
   return {
     loading,
+    themeMode,
   };
 };
 export default connect(mapStateToProps)(Spinner);
